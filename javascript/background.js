6af8f194122f37dd6bd2e558db2e5c54418589a5
David Oren <david_oren@yahoo.com>
2012-07-03 22:27:19 +0300

    Allow profile settings to be synced between computers.

--- javascript/background.js
+++ javascript/background.js
@@ -10,3 +10,37 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     }
     sendResponse(response);
 });
+
+function updateSyncedProfiles(data) {
+    localStorage["synced_profiles_keys"] = "";
+    if (data.synced_profiles == undefined) {
+      data.synced_profiles = "";
+    } else if (typeof(data.synced_profiles) != "string") {
+      profiles = "";
+      for (i in data.synced_profiles) {
+        profiles = profiles + data[data.synced_profiles[i]];
+      }
+      localStorage["synced_profiles_keys"] = data.synced_profiles.join();
+      data.synced_profiles = profiles;
+    }
+    localStorage["synced_profiles"] = data.synced_profiles;
+}
+
+chrome.storage.sync.get(null, function(data) {
+    if (chrome.extension.lastError == undefined) {
+        updateSyncedProfiles(data);
+    }
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    if (namespace != "sync") {
+        return;
+    }
+    if (changes.synced_profiles != undefined) {
+        var flattened = {};
+        for (i in changes) {
+          flattened[i] = changes[i].newValue;
+        }
+        updateSyncedProfiles(flattened);
+    }
+});
