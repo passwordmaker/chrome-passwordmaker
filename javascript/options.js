f73f6e9dcee5f84358bf03cb13644885b347efd1
Steve Allison <github.commit@stalks.nooblet.org>
2013-01-03 00:51:12 +0000

    importRdf() changes allow user to delete profiles
    importRdf() sets profiles to empty array [] prior to import if checkbox
    is ticked and profiles are ready to import

--- javascript/options.js
+++ javascript/options.js
@@ -132,6 +132,14 @@ function importRdf(){
 
     var rdfDoc = RdfImporter.loadDoc(txt);
 
+    // Check that profiles have been parsed and are available before wiping current data
+    if ((rdfDoc && rdfDoc.profiles && rdfDoc.profiles.length) && ($('#inputImportOverwrite').attr('checked') == true)) {
+        console.log("set null");
+        // Setting to null triggers creation of default profile, settings to empty array bypasses that code ([] != null)
+        Settings.profiles = [];
+        Settings.saveProfiles();
+    }
+    
     var count = RdfImporter.saveProfiles(rdfDoc.profiles);
 
     if(!count){
