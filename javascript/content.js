cd1ade4bd320504d1ea370c2ba9e932b1574521c
Eric Aguiar <ultimate.evil@gmail.com>
2014-05-20 12:28:34 -0400

    Trying to use jQuery on content.js again for issue #107 or if calling a separate function inside the message handler to create a new variable scope is needed.

--- javascript/content.js
+++ javascript/content.js
@@ -3,18 +3,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         fillPasswords(request.password);
     }
     if (request.hasPasswordField) {
-        if (document.querySelector("input[type=password]") !== null) {
+        if (hasPassFields()) {
             sendResponse({hasField: true});
         }
     }
 });
 
 function fillPasswords(password) {
-    var passFields = document.querySelectorAll("input[type=password]");
-    [].forEach.call(passFields, function (element, index) {
-        // Only fill password fields that are empty and aren't already populated (for change password pages)
-        if (passFields[index].value.length === 0) {
-            passFields[index].value = password;
-        }
-    });
+    jQuery("input[type=password]").val(password);
+}
+
+function hasPassFields() {
+  var fields = jQuery("input[type=password]");
+  hasFields = false;
+  if (fields && fields.length > 0) {
+      hasFields = true;
+  }
+  return hasFields;
 }
