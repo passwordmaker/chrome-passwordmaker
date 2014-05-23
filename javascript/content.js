53e08e7dec5feb8731d8a3346b5819b8a1b2b786
Eric Aguiar <ultimate.evil@gmail.com>
2014-05-23 09:50:11 -0400

    Use jQuery again in content script, attempt #2 to fix Fill Field button.

--- javascript/content.js
+++ javascript/content.js
@@ -4,24 +4,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
     if (request.hasPasswordField) {
         if (hasPassFields()) {
-            sendResponse({hasField: true});
+          sendResponse({hasField: true});
         }
     }
 });
 
 function fillPasswords(password) {
-    var fields = document.querySelectorAll("input[type='password']");
-    [].forEach.call(fields, function (element, index) {
-        // Only fill password fields that are empty and aren't already populated (for change password pages)
-        if (fields[index].value.length === 0) {
-            fields[index].value = password.toString();
-        }
-    });
+    jQuery("input[type=password]").val(password);
 }
 
 function hasPassFields() {
-    var hasFields = false;
-    if (document.querySelector("input[type='password']") !== null) {
+    var fields = jQuery("input[type=password]");
+    hasFields = false;
+    if (fields.length > 0) {
         hasFields = true;
     }
     return hasFields;
