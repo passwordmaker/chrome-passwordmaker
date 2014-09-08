f0e9c0731d501a57aa955e63e80dc9e7ee3857c3
Eric Aguiar <ultimate.evil@gmail.com>
2014-09-08 18:43:04 -0400

    Update QUnit to 1.15.0 Updated test setup to current best-practice. Explicitly use QUnit and assert.

--- test/import.js
+++ test/import.js
@@ -1,178 +1,187 @@
-module("rdf import");
-
-var rdf_doc1 = null;
-QUnit.testStart = function() {
-    localStorage.clear();
-    Settings.profiles = [];
-    rdf_doc1 = RdfImporter.loadDoc($("#rdf1").val());
-};
-
-test("parse global settings", function() {
-    var s = rdf_doc1.settings;
-
-    deepEqual(s.rdf_about, "http://passwordmaker.mozdev.org/globalSettings");
-    deepEqual(s.hideMasterPassword, false);
-    deepEqual(s.storeLocation, "memory");
+QUnit.module("rdf import", {
+    setup: function() {
+        this.rdf_doc1 = RdfImporter.loadDoc($("#rdf1").val());
+    },
+    teardown: function() {
+        Settings.profiles = [];
+        localStorage.clear();
+    }
 });
 
-test("find profiles", function() {
-    deepEqual(rdf_doc1.profiles.length, 2);
+QUnit.test("parse global settings", function(assert) {
+    var s = this.rdf_doc1.settings;
+
+    assert.equal(s.rdf_about, "http://passwordmaker.mozdev.org/globalSettings");
+    assert.equal(s.hideMasterPassword, false);
+    assert.equal(s.storeLocation, "memory");
 });
 
-test("load profile", function() {
-    var p = rdf_doc1.profiles[1];
-
-    deepEqual(p.rdf_about, "rdf:#$5PGpU1");
-    deepEqual(p.title, "nospecial");
-    deepEqual(p.url_protocol, false);
-    deepEqual(p.url_subdomain, false);
-    deepEqual(p.url_domain, true);
-    deepEqual(p.url_path, true);
-    deepEqual(p.hashAlgorithm, "hmac-sha256_fix");
-    deepEqual(p.username, "username1");
-    deepEqual(p.modifier, "modifier1");
-    deepEqual(p.passwordLength, 20);
-    deepEqual(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-    deepEqual(p.passwordPrefix, "prefix1");
-    deepEqual(p.passwordSuffix, "suffix1");
-    deepEqual(p.whereToUseL33t, "before-hashing");
-    deepEqual(p.l33tLevel, 1);
-    deepEqual(p.siteList, "/https?://mail\\.yahoo\\.com/.*/ http?://github.com/*");
+QUnit.test("find profiles", function(assert) {
+    assert.equal(this.rdf_doc1.profiles.length, 2);
 });
 
-test("load default profile", function() {
-    var p = rdf_doc1.profiles[0];
+QUnit.test("load profile", function(assert) {
+    var p = this.rdf_doc1.profiles[1];
 
-    deepEqual(p.rdf_about, "http://passwordmaker.mozdev.org/defaults");
-    deepEqual(p.title, "Defaults");
-    deepEqual(p.url_protocol, false);
-    deepEqual(p.url_subdomain, false);
-    deepEqual(p.url_domain, true);
-    deepEqual(p.url_path, true);
-    deepEqual(p.hashAlgorithm, "sha256");
-    deepEqual(p.username, "username1");
-    deepEqual(p.modifier, "modifier1");
-    deepEqual(p.passwordLength, 15);
-    deepEqual(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%");
-    deepEqual(p.passwordPrefix, "prefix1");
-    deepEqual(p.passwordSuffix, "suffix1");
-    deepEqual(p.whereToUseL33t, "off");
-    deepEqual(p.l33tLevel, 1);
-    deepEqual(p.siteList, "");
+    assert.equal(p.rdf_about, "rdf:#$5PGpU1");
+    assert.equal(p.title, "nospecial");
+    assert.equal(p.url_protocol, false);
+    assert.equal(p.url_subdomain, false);
+    assert.equal(p.url_domain, true);
+    assert.equal(p.url_path, true);
+    assert.equal(p.hashAlgorithm, "hmac-sha256_fix");
+    assert.equal(p.username, "username1");
+    assert.equal(p.modifier, "modifier1");
+    assert.equal(p.passwordLength, 20);
+    assert.equal(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    assert.equal(p.passwordPrefix, "prefix1");
+    assert.equal(p.passwordSuffix, "suffix1");
+    assert.equal(p.whereToUseL33t, "before-hashing");
+    assert.equal(p.l33tLevel, 1);
+    assert.equal(p.siteList, "/https?://mail\\.yahoo\\.com/.*/ http?://github.com/*");
 });
 
-test("save profiles", function() {
-    var profiles = rdf_doc1.profiles;
+QUnit.test("load default profile", function(assert) {
+    var p = this.rdf_doc1.profiles[0];
 
-    deepEqual(profiles.length, 2);
+    assert.equal(p.rdf_about, "http://passwordmaker.mozdev.org/defaults");
+    assert.equal(p.title, "Defaults");
+    assert.equal(p.url_protocol, false);
+    assert.equal(p.url_subdomain, false);
+    assert.equal(p.url_domain, true);
+    assert.equal(p.url_path, true);
+    assert.equal(p.hashAlgorithm, "sha256");
+    assert.equal(p.username, "username1");
+    assert.equal(p.modifier, "modifier1");
+    assert.equal(p.passwordLength, 15);
+    assert.equal(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%");
+    assert.equal(p.passwordPrefix, "prefix1");
+    assert.equal(p.passwordSuffix, "suffix1");
+    assert.equal(p.whereToUseL33t, "off");
+    assert.equal(p.l33tLevel, 1);
+    assert.equal(p.siteList, "");
+});
+
+QUnit.test("save profiles", function(assert) {
+    var profiles = this.rdf_doc1.profiles;
+
+    assert.equal(profiles.length, 2);
     Settings.loadProfiles();
-    deepEqual(Settings.profiles.length, 2);
+    assert.equal(Settings.profiles.length, 2);
 
     RdfImporter.saveProfiles(profiles);
-
-    deepEqual(Settings.profiles.length, 4);
+    assert.equal(Settings.profiles.length, 4);
 });
 
-test("save settings", function() {
+QUnit.test("save settings", function(assert) {
     localStorage.setItem("show_generated_password", true);
     Settings.setStoreLocation("memory");
 
-    deepEqual(Settings.shouldHidePassword(), true);
-    deepEqual(Settings.storeLocation, "memory");
+    assert.equal(Settings.shouldHidePassword(), true);
+    assert.equal(Settings.storeLocation, "memory");
 });
 
-module("rdf export");
-
-test("dump profile to rdf", function() {
-    Settings.loadProfiles();
-    RdfImporter.saveProfiles(rdf_doc1.profiles);
-    deepEqual(Settings.profiles.length, 4);
-    var doc2 = RdfImporter.loadDoc(RdfImporter.dumpDoc());
-
-    var p = doc2.profiles[3];
-    deepEqual(p.rdf_about, "rdf:#$CHROME4");
-    deepEqual(p.title, "nospecial");
-    deepEqual(p.url_protocol, false);
-    deepEqual(p.url_subdomain, false);
-    deepEqual(p.url_domain, true);
-    deepEqual(p.url_path, true);
-    deepEqual(p.hashAlgorithm, "hmac-sha256_fix");
-    deepEqual(p.username, "username1");
-    deepEqual(p.modifier, "modifier1");
-    deepEqual(p.passwordLength, 20);
-    deepEqual(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-    deepEqual(p.passwordPrefix, "prefix1");
-    deepEqual(p.passwordSuffix, "suffix1");
-    deepEqual(p.whereToUseL33t, "before-hashing");
-    deepEqual(p.l33tLevel, 1);
-    deepEqual(p.siteList, "/https?://mail\\.yahoo\\.com/.*/ http?://github.com/*");
+QUnit.module("rdf export", {
+    setup: function() {
+        Settings.loadProfiles();
+        this.rdf_doc1 = RdfImporter.loadDoc($("#rdf1").val());
+        RdfImporter.saveProfiles(this.rdf_doc1.profiles);
+        this.doc2 = RdfImporter.loadDoc(RdfImporter.dumpDoc());
+    },
+    teardown: function() {
+        Settings.profiles = [];
+        localStorage.clear();
+    }
 });
 
-test("dump default profile to rdf", function() {
-    Settings.loadProfiles();
-    RdfImporter.saveProfiles(rdf_doc1.profiles);
-    deepEqual(Settings.profiles.length, 4);
-    var doc2 = RdfImporter.loadDoc(RdfImporter.dumpDoc());
+QUnit.test("dump profile to rdf", function(assert) {
+    var p = this.doc2.profiles[3];
 
-    var p = doc2.profiles[2];
-
-    deepEqual(p.rdf_about, "rdf:#$CHROME3");
-    deepEqual(p.title, "Defaults");
-    deepEqual(p.url_protocol, false);
-    deepEqual(p.url_subdomain, false);
-    deepEqual(p.url_domain, true);
-    deepEqual(p.url_path, true);
-    deepEqual(p.hashAlgorithm, "sha256");
-    deepEqual(p.username, "username1");
-    deepEqual(p.modifier, "modifier1");
-    deepEqual(p.passwordLength, 15);
-    deepEqual(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%");
-    deepEqual(p.passwordPrefix, "prefix1");
-    deepEqual(p.passwordSuffix, "suffix1");
-    deepEqual(p.whereToUseL33t, "off");
-    deepEqual(p.l33tLevel, 1);
-    deepEqual(p.siteList, "");
+    assert.equal(p.rdf_about, "rdf:#$CHROME4");
+    assert.equal(p.title, "nospecial");
+    assert.equal(p.url_protocol, false);
+    assert.equal(p.url_subdomain, false);
+    assert.equal(p.url_domain, true);
+    assert.equal(p.url_path, true);
+    assert.equal(p.hashAlgorithm, "hmac-sha256_fix");
+    assert.equal(p.username, "username1");
+    assert.equal(p.modifier, "modifier1");
+    assert.equal(p.passwordLength, 20);
+    assert.equal(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    assert.equal(p.passwordPrefix, "prefix1");
+    assert.equal(p.passwordSuffix, "suffix1");
+    assert.equal(p.whereToUseL33t, "before-hashing");
+    assert.equal(p.l33tLevel, 1);
+    assert.equal(p.siteList, "/https?://mail\\.yahoo\\.com/.*/ http?://github.com/*");
 });
 
-module("password generation");
+QUnit.test("dump default profile to rdf", function(assert) {
+    var p = this.doc2.profiles[2];
 
-test("algorithms", function() {
-    Settings.loadProfiles();
-    var p = Settings.profiles[0];
-    var url = "http://www.passwordmaker.org/";
-    var pass = "i♥passwordMaker©";
-    p.hashAlgorithm = "md4";
-    deepEqual(btoa(p.getPassword(url, pass)), "Qm4kQn10bSw=", "MD4 variant");
-    p.hashAlgorithm = "hmac-md4";
-    deepEqual(btoa(p.getPassword(url, pass)), "R2hwJUFmYFw=", "HMAC-MD4 variant");
-    p.hashAlgorithm = "md5";
-    deepEqual(btoa(p.getPassword(url, pass)), "SEUxMmc/b0c=", "MD5 variant");
-    p.hashAlgorithm = "md5_v6";
-    deepEqual(btoa(p.getPassword(url, pass)), "NWFiNTlmMzk=", "MD5 Version 0.6 variant");
-    p.hashAlgorithm = "hmac-md5";
-    deepEqual(btoa(p.getPassword(url, pass)), "dH5fd1hcJnQ=", "HMAC-MD5 variant");
-    p.hashAlgorithm = "hmac-md5_v6";
-    deepEqual(btoa(p.getPassword(url, pass)), "OGUzMGJhZTA=", "HMAC-MD5 Version 0.6 variant");
-    p.hashAlgorithm = "sha1";
-    deepEqual(btoa(p.getPassword(url, pass)), "Rn4vcGIuQGE=", "SHA-1 variant");
-    p.hashAlgorithm = "hmac-sha1";
-    deepEqual(btoa(p.getPassword(url, pass)), "NTJxZklgPn0=", "HMAC-SHA-1 variant");
-    p.hashAlgorithm = "sha256";
-    deepEqual(btoa(p.getPassword(url, pass)), "TzA5Qlp9PT4=", "SHA-256 variant");
-    p.hashAlgorithm = "hmac-sha256_fix";
-    deepEqual(btoa(p.getPassword(url, pass)), "OW9QUzdQPnU=", "HMAC-SHA-256 variant");
-    p.hashAlgorithm = "hmac-sha256";
-    deepEqual(btoa(p.getPassword(url, pass)), "VlFDa0E/Qyg=", "HMAC-SHA-256 Version 1.5.1 variant");
-    p.hashAlgorithm = "rmd160";
-    deepEqual(btoa(p.getPassword(url, pass)), "R21MWT17dCc=", "RIPEMD-160 variant");
-    p.hashAlgorithm = "hmac-rmd160";
-    deepEqual(btoa(p.getPassword(url, pass)), "KE0+Tk1WVyM=", "HMAC-RIPEMD-160 variant");
-    p.l33tLevel = 9;
-    p.hashAlgorithm = "md5";
-    p.whereToUseL33t = "before-hashing";
-    deepEqual(btoa(p.getPassword(url, pass)), "SzpLelh4I1c=", "Before L33t variant");
-    p.whereToUseL33t = "after-hashing";
-    deepEqual(btoa(p.getPassword(url, pass)), "fC18JjEyNj8=", "After L33t variant");
-    p.whereToUseL33t = "both";
-    deepEqual(btoa(p.getPassword(url, pass)), "fHs6fHsiL18=", "Befor & After L33t variant");
+    assert.equal(p.rdf_about, "rdf:#$CHROME3");
+    assert.equal(p.title, "Defaults");
+    assert.equal(p.url_protocol, false);
+    assert.equal(p.url_subdomain, false);
+    assert.equal(p.url_domain, true);
+    assert.equal(p.url_path, true);
+    assert.equal(p.hashAlgorithm, "sha256");
+    assert.equal(p.username, "username1");
+    assert.equal(p.modifier, "modifier1");
+    assert.equal(p.passwordLength, 15);
+    assert.equal(p.selectedCharset, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%");
+    assert.equal(p.passwordPrefix, "prefix1");
+    assert.equal(p.passwordSuffix, "suffix1");
+    assert.equal(p.whereToUseL33t, "off");
+    assert.equal(p.l33tLevel, 1);
+    assert.equal(p.siteList, "");
+});
+
+QUnit.module("password generation", {
+    setup: function() {
+        Settings.loadProfiles();
+        this.p = Settings.profiles[0];
+        this.url = "http://www.passwordmaker.org/";
+        this.pass = "i♥passwordMaker©";
+    },
+    teardown: function() {
+        Settings.profiles = [];
+        localStorage.clear();
+    }
+});
+
+QUnit.test("algorithms", function(assert) {
+    this.p.hashAlgorithm = "md4";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "Qm4kQn10bSw=", "MD4 variant");
+    this.p.hashAlgorithm = "hmac-md4";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "R2hwJUFmYFw=", "HMAC-MD4 variant");
+    this.p.hashAlgorithm = "md5";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "SEUxMmc/b0c=", "MD5 variant");
+    this.p.hashAlgorithm = "md5_v6";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "NWFiNTlmMzk=", "MD5 Version 0.6 variant");
+    this.p.hashAlgorithm = "hmac-md5";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "dH5fd1hcJnQ=", "HMAC-MD5 variant");
+    this.p.hashAlgorithm = "hmac-md5_v6";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "OGUzMGJhZTA=", "HMAC-MD5 Version 0.6 variant");
+    this.p.hashAlgorithm = "sha1";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "Rn4vcGIuQGE=", "SHA-1 variant");
+    this.p.hashAlgorithm = "hmac-sha1";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "NTJxZklgPn0=", "HMAC-SHA-1 variant");
+    this.p.hashAlgorithm = "sha256";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "TzA5Qlp9PT4=", "SHA-256 variant");
+    this.p.hashAlgorithm = "hmac-sha256_fix";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "OW9QUzdQPnU=", "HMAC-SHA-256 variant");
+    this.p.hashAlgorithm = "hmac-sha256";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "VlFDa0E/Qyg=", "HMAC-SHA-256 Version 1.5.1 variant");
+    this.p.hashAlgorithm = "rmd160";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "R21MWT17dCc=", "RIPEMD-160 variant");
+    this.p.hashAlgorithm = "hmac-rmd160";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "KE0+Tk1WVyM=", "HMAC-RIPEMD-160 variant");
+    this.p.l33tLevel = 9;
+    this.p.hashAlgorithm = "md5";
+    this.p.whereToUseL33t = "before-hashing";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "SzpLelh4I1c=", "Before L33t variant");
+    this.p.whereToUseL33t = "after-hashing";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "fC18JjEyNj8=", "After L33t variant");
+    this.p.whereToUseL33t = "both";
+    assert.equal(btoa(this.p.getPassword(this.url, this.pass)), "fHs6fHsiL18=", "Befor & After L33t variant");
 });
